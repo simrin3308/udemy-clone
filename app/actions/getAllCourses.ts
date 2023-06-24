45694790dac6806c6a022b89a78dbd34879cfdb4
Simrin Pal Singh <codingwithsaam@gmail.com>
2023-06-24 12:20:17 +0530

    get all courses

---
+++ app/actions/getAllCourses.ts
@@ -0,0 +1,32 @@
+import prisma from "../libs/prismadb";
+
+export default async function getAllCourses(params: any) {
+  try {
+    const { result } = params;
+    console.log(params);
+    console.log(result);
+
+    let query: any = {};
+    if (result) {
+      query.name = {
+        contains: result,
+      };
+    }
+
+    const allCourses = await prisma.course.findMany({
+      where: query,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const safeCourse = allCourses.map((course: any) => ({
+      ...course,
+      createdAt: course.createdAt.toISOstring,
+    }));
+
+    return safeCourse;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import SliderMain from "./(components)/Slider/SliderMenu";
+import getAllCourses from "./actions/getAllCourses";
+import CourseComponent from "./(components)/CourseComponent/CourseComponent";
 
 const images = ["/a.jpg", "b.jpg"];
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: string;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
   const session = await getServerSession(authOptions);
+  const courses = await getAllCourses(searchParams);
   return (
     <main>
       <SliderMain images={images} />
+
+      <div>
+        <div className="flex flex-wrap px-8">
+          {courses.map((item: any) => (
+            <CourseComponent key={item.id} data={item} />
+          ))}
+        </div>
+      </div>
+
       <h1>Home</h1>
       <h1>SSR</h1>
       <h1>{JSON.stringify(session)}</h1>
